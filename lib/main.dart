d5433ae7efde03bae060ae959bd925845a81a54a
peactor <peactor@gmail.com>
2022-01-05 06:13:16 +0900

    SingleChildScrollview

--- lib/main.dart
+++ lib/main.dart
@@ -16,39 +16,26 @@ class MyApp extends StatelessWidget {
 }
 
 
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final items = List.generate(100, (i) => i+1).toList();
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('제목'),
       ),
-      body: Stack(
-
-        children: <Widget>[
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-          ),
-          Container(
-            color: Colors.green,
-            width: 90,
-            height: 90,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-          ),
-          Container(
-            color: Colors.blue,
-            width: 80,
-            height: 80,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-          ),
-        ],
+      body:
+      SingleChildScrollView(
+        child: ListBody(
+          children: items.map((i) => Text('$i')).toList(),
+        ),
       ),
+
+
+
     );
   }
 }
